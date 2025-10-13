4045c3e542c1e4bddfe38dce50fa608f694c288e
Akshit1025 <akshitthecoder@gmail.com>
2025-10-13 22:56:07 +0530

    removed test page

--- lib/repository/Youtube/yt_music_api.dart
+++ lib/repository/Youtube/yt_music_api.dart
@@ -22,7 +22,7 @@ class YtMusicService {
   static const baseApiEndpoint = '/youtubei/v1/';
   static const ytmParams = {
     'alt': 'json',
-    'key': 'AIzaSyC9XL3ZjWddXya6X74dJoCTL-WEYFDNX30',
+    'key': 'AIzaSyBtbK6tKZkSp28LyWi1TXJNlKzNqJzbrxg',
   };
   static const userAgent =
       'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0';

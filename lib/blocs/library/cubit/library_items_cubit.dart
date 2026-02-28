3bd3b6927e8c89c5d5f5a3e42bb4e2e8ed422353
Akshit1025 <akshitthecoder@gmail.com>
2026-03-01 00:00:04 +0530

    library repeated list fix

--- lib/blocs/library/cubit/library_items_cubit.dart
+++ lib/blocs/library/cubit/library_items_cubit.dart
@@ -14,66 +14,67 @@ part 'library_items_state.dart';
 class LibraryItemsCubit extends Cubit<LibraryItemsState> {
   MediaDBCubit mediaDBCubit;
   List<MediaPlaylist> mediaPlaylist = [];
-  LibraryItemsState libraryItemsState = LibraryItemsState(playlists: List.empty(growable: true));
+  LibraryItemsState libraryItemsState =
+  LibraryItemsState(playlists: List.empty(growable: true));
 
   LibraryItemsCubit({required this.mediaDBCubit})
-    : super(LibraryItemsInitial()) {
-    mediaDBCubit.refreshLibrary.listen((value) {
-      log(value.toString(), name: "libItemsCubit");
-      if (value) {
-        getAndEmitPlaylists();
-        log("got refresh command", name: "libItemsCubit");
-      }
-    });
+      : super(LibraryItemsInitial()) {
+    mediaDBCubit.refreshLibrary.listen(
+          (value) {
+        log(value.toString(), name: "libItemsCubit");
+        if (value) {
+          getAndEmitPlaylists();
+          log("got refresh command", name: "libItemsCubit");
+        }
+      },
+    );
     getAndEmitPlaylists();
   }
 
   Future<void> getAndEmitPlaylists() async {
-    libraryItemsState = LibraryItemsState(
-      playlists: List.empty(growable: true),
-    );
+    List<PlaylistItemProperties> _playlists = List.empty(growable: true);
+    libraryItemsState =
+        LibraryItemsState(playlists: List.empty(growable: true));
 
     mediaPlaylist = await mediaDBCubit.getListOfPlaylists2();
 
-    List<String> _playlists = List.empty(growable: true);
+    // List<String> _playlists = List.empty(growable: true);
 
-    if (libraryItemsState.playlists.isNotEmpty) {
-      for (var element in libraryItemsState.playlists) {
-        _playlists.add(element.playlistName ?? "Unknown");
-      }
-      libraryItemsState.playlists = [];
-    }
+    // if (libraryItemsState.playlists.isNotEmpty) {
+    //   for (var element in libraryItemsState.playlists) {
+    //     _playlists.add(element.playlistName ?? "Unknown");
+    //   }
+    //   libraryItemsState.playlists = [];
+    // }
+    libraryItemsState.playlists.clear();
+    log("emitted from library0 ${_playlists.length} - MediaPlaylists ${mediaPlaylist.length}",
+        name: "libItemsCubit");
+
     if (mediaPlaylist.length > 0) {
       for (var element in mediaPlaylist) {
-        // if (_playlists.contains(element.albumName)) {
-        //   int? _idx = _playlists.indexOf(element.albumName);
-
-        //   libraryItemsState.playlists.removeAt(_idx);
-        // }
         ImageProvider _tempProvider;
 
         if (element.mediaItems.length > 0) {
-          _tempProvider = await getImageProvider(
-            element.mediaItems[0].artUri.toString(),
-          );
+          _tempProvider =
+          await getImageProvider(element.mediaItems[0].artUri.toString());
         } else {
           _tempProvider = await getImageProvider("");
         }
         PlaylistItemProperties _playlistItem = PlaylistItemProperties(
-          playlistName: element.albumName,
-          imageProvider: _tempProvider,
-          subTitle: "${element.mediaItems.length} Items",
-        );
-        libraryItemsState.playlists.add(_playlistItem);
+            playlistName: element.albumName,
+            imageProvider: _tempProvider,
+            subTitle: "${element.mediaItems.length} Items");
+        // libraryItemsState.playlists.add(_playlistItem);
+        _playlists.add(_playlistItem);
 
         // libraryItemsState.playlistNames?.add(element.albumName);
         // libraryItemsState.subTitles?.add("Saavan");
       }
-      emit(libraryItemsState);
-      log(
-        "emitted from library ${_playlists.toString()} - ${libraryItemsState.playlists.length} - MediaPlaylists ${mediaPlaylist}",
-        name: "libItemsCubit"
-      );
+      if (_playlists.length == mediaPlaylist.length) {
+        emit(state.copyWith(playlists: _playlists));
+      }
+      log("emitted from library ${_playlists.length} - MediaPlaylists ${mediaPlaylist.length}",
+          name: "libItemsCubit");
     }
   }
 

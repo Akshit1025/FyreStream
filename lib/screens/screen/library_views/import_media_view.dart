a362872e73e3f8431d2d890418771088b5fdd3fc
Akshit1025 <akshitthecoder@gmail.com>
2026-09-25 20:49:42 +0530

    feat: refreshed import page

--- lib/screens/screen/library_views/import_media_view.dart
+++ lib/screens/screen/library_views/import_media_view.dart
@@ -118,23 +118,21 @@ class ImportFromBtn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextButton.icon(
-      onPressed: () {
-        onClickFunc();
-      },
-      icon: Padding(
-        padding: const EdgeInsets.only(left: 8, right: 8),
-        child: Icon(
-          btnIcon,
-          color: Default_Theme.primaryColor1,
-          size: 30
-        ),
-      ),
-      label: Text(
+    return ListTile(
+      splashColor: Default_Theme.accentColor1.withOpacity(0.3),
+      onTap: onClickFunc,
+      title: Text(
         btnName,
-        style: Default_Theme.secondoryTextStyle.merge(
-          const TextStyle(fontSize: 20, color: Default_Theme.primaryColor1),
-        ),
+        style: const TextStyle(
+          color: Default_Theme.primaryColor1,
+          fontSize: 20,
+          fontWeight: FontWeight.bold
+        ).merge(Default_Theme.secondoryTextStyle),
+      ),
+      leading: Icon(
+        btnIcon,
+        color: Default_Theme.primaryColor1,
+        size: 25,
       ),
     );
   }
